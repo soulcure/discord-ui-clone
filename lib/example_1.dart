72a0df7eb4ea2be6cb151b10b70bdeb573543728
soulcure <soulcure.chen@idreamsky.com>
2021-05-19 12:51:33 +0800

    innerDrawer update

---
+++ lib/example_1.dart
@@ -0,0 +1,59 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'Screens/InnerDrawer.dart';
+import 'children/left_child_1.dart';
+import 'notifier/drawer_notifier.dart';
+import 'children/right_child_1.dart';
+import 'scaffolds/scaffolds_1.dart';
+
+class ExampleOne extends StatelessWidget {
+  final GlobalKey<InnerDrawerState> _innerDrawerKey =
+      GlobalKey<InnerDrawerState>();
+
+  String _title = "One";
+
+  Color pickerColor = Color(0xff443a49);
+  Color currentColor = Colors.black54;
+  ValueChanged<Color> onColorChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    print("example 1");
+    //final drawerNotifier = Provider.of<DrawerNotifier>(context, listen: true);
+
+    return InnerDrawer(
+      key: _innerDrawerKey,
+      onTapClose: context.select((DrawerNotifier value) => value.onTapToClose),
+      tapScaffoldEnabled:
+          context.select((DrawerNotifier value) => value.tapScaffold),
+      velocity: 20,
+      swipeChild: true,
+      offset: IDOffset.horizontal(
+          context.select((DrawerNotifier value) => value.offset)),
+      swipe: context.select((DrawerNotifier value) => value.swipe),
+      colorTransitionChild:
+          context.select((DrawerNotifier value) => value.colorTransition),
+      colorTransitionScaffold:
+          context.select((DrawerNotifier value) => value.colorTransition),
+      leftAnimationType:
+          context.select((DrawerNotifier value) => value.animationType),
+      rightAnimationType: InnerDrawerAnimation.linear,
+
+      leftChild: LeftChild(),
+
+      rightChild: RightChild(innerDrawerKey: _innerDrawerKey),
+
+      scaffold: ScaffoldDrawer(innerDrawerKey: _innerDrawerKey),
+
+      onDragUpdate: (double value, InnerDrawerDirection direction) {
+        //BAD: DO NOT DO THIS, take it as a general example.
+        // We working to find a solution.
+        //drawerNotifier.setSwipeOffset(value);
+        context.read<DrawerNotifier>().setSwipeOffset(value);
+      },
+      //innerDrawerCallback: (a) => print(a),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,211 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(MyApp());
-}
+import 'example_1.dart';
+import 'example_2.dart';
+import 'example_3.dart';
+import 'notifier/drawer_notifier.dart';
+
+void main() => runApp(ChangeNotifierProvider(
+      create: (context) => DrawerNotifier(),
+      child: MyApp(),
+    ));
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Inner Drawer',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blueGrey,
+        backgroundColor: Colors.white,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MainApp(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+enum Example { one, two, three }
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MainApp extends StatefulWidget {
+  MainApp({Key key}) : super(key: key);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MainAppState createState() => _MainAppState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MainAppState extends State<MainApp> with SingleTickerProviderStateMixin {
+  bool isOpened = false;
+  AnimationController _animationController;
+  Animation<Color> _buttonColor;
+  Animation<double> _animateIcon;
+  Animation<double> _translateButton;
+  Curve _curve = Curves.easeOut;
+  double _fabHeight = 56.0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  Example _currentExample = Example.one;
+
+  @override
+  initState() {
+    super.initState();
+
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      //systemNavigationBarColor: Colors.blue,
+      statusBarColor: Colors.transparent,
+      //statusBarBrightness: Brightness.dark,
+    ));
+
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 500))
+          ..addListener(() {
+            setState(() {});
+          });
+    _animateIcon =
+        Tween<double>(begin: 0.0, end: 1.0).animate(_animationController);
+    _buttonColor = ColorTween(
+      begin: Colors.black45,
+      end: Colors.red,
+    ).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Interval(
+        0.00,
+        1.00,
+        curve: Curves.linear,
+      ),
+    ));
+    _translateButton = Tween<double>(
+      begin: _fabHeight,
+      end: -14.0,
+    ).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Interval(
+        0.0,
+        0.75,
+        curve: _curve,
+      ),
+    ));
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return Stack(
+      children: <Widget>[
+        _switchWidget(_currentExample),
+        Container(
+          padding: EdgeInsets.all(20),
+          alignment: Alignment.bottomRight,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Transform(
+                transform: Matrix4.translationValues(
+                  0.0,
+                  _translateButton.value * 3,
+                  0.0,
+                ),
+                child: _item(title: "One", example: Example.one),
+              ),
+              Transform(
+                transform: Matrix4.translationValues(
+                  0.0,
+                  _translateButton.value * 2,
+                  0.0,
+                ),
+                child: _item(title: "Two", example: Example.two),
+              ),
+              Transform(
+                transform: Matrix4.translationValues(
+                  0.0,
+                  _translateButton.value * 1,
+                  0.0,
+                ),
+                child: _item(title: "Three", example: Example.three),
+              ),
+              _toggle(),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _switchWidget(Example example) {
+    switch (example) {
+      case Example.one:
+        return ExampleOne();
+        break;
+      case Example.two:
+        return ExampleTwo();
+        break;
+      case Example.three:
+        return ExampleThree();
+        break;
+    }
+  }
+
+  Widget _item({String title, Example example}) {
+    //print(((_translateButton.value-66)/60).abs());
+    double val = ((_translateButton.value - 56) / 60).abs();
+    Color color;
+    switch (example) {
+      case Example.one:
+        color = Colors.green[300];
+        break;
+      case Example.two:
+        color = Colors.orange[300];
+        break;
+      default:
+        color = Colors.blue[300];
+    }
+
+    return Opacity(
+      opacity: val > 0 ? 1 : 0,
+      child: FloatingActionButton(
+        backgroundColor: Colors.white,
+        elevation: val,
+        onPressed: () {
+          setState(() => _currentExample = example);
+          _animate();
+        },
+        tooltip: 'Apri',
+        child: Text(title,
+            style: TextStyle(
+              fontSize: 11,
+              color: color,
+            )),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    );
+  }
+
+  Widget _toggle() {
+    return new Container(
+      child: FloatingActionButton(
+        elevation: 1.5,
+        backgroundColor: Colors.white,
+        onPressed: _animate,
+        tooltip: 'Toggle',
+        child: AnimatedIcon(
+          icon: AnimatedIcons.menu_close,
+          color: _buttonColor.value,
+          progress: _animateIcon,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  void _animate() {
+    if (Navigator.of(context).canPop()) Navigator.of(context).pop();
+    if (!isOpened)
+      _animationController.forward();
+    else
+      _animationController.reverse();
+    isOpened = !isOpened;
   }
 }
